c69ec10df944c70119fa97c962fba8ef8c45b907
vincent <vincent.guibert79@gmail.com>
2022-09-28 18:04:17 +0200

    Add item path

--- controllers/items.js
+++ controllers/items.js
@@ -16,7 +16,7 @@ exports.getAllItems = (req, res, next) => {
 
 exports.createItems = (req, res, next) => {
     if (!req.body) return next(new AppError("No form data found", 404));
-    const values = [req.body.nameCara, req.body.classe, req.body.race, req.body.levelCara, req.body.lore];
+    const values = [req.body.nameItem, req.body.Description];
     console.log(values);
     conn.query(
       "INSERT INTO items (Name, Description) VALUES(?)",
